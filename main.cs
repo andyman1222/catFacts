80a3bb019982723e00b2f89546aedb02fb7c1f24
Andy Herbert <andycherbert@gmail.com>
2017-07-24 21:39:29 -0700

    cat.

---
+++ main.cs
@@ -0,0 +1,32 @@
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System;
+
+class main{
+    static string getCatFact(){
+        HttpWebRequest rq = (HttpWebRequest)WebRequest.Create("https://catfact.ninja/fact");
+        rq.Method = "GET";
+        HttpWebResponse resp = (HttpWebResponse)rq.GetResponse();
+        using(StreamReader s = new StreamReader(resp.GetResponseStream())) {
+            for(int i = 0; i < 9; i++){
+                s.Read();
+            }
+            string output = "";
+            char ch = (char)s.Read();
+            while (ch != '\"'){
+                output += ch;
+                ch = (char)s.Read();
+            }
+            return output;
+        }
+    }
+    [STAThread]
+    public static void Main(){
+        while(true){
+            Clipboard.SetText(getCatFact());
+            Thread.Sleep(500);
+        }
+    }
+}
