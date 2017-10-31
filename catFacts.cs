04b99bf85d57c66eed9292ffaac9b768392757f5
Andy Herbert <andycherbert@gmail.com>
2017-10-30 21:49:23 -0700

    cat

--- catFacts.cs
+++ catFacts.cs
@@ -4,31 +4,20 @@ using System.IO;
 using System.Threading;
 using System;
 
+
 class catFacts{
     static string getCatFact(){
-        HttpWebRequest rq = (HttpWebRequest)WebRequest.Create("https://catfact.ninja/fact");
-        rq.Method = "GET";
-        HttpWebResponse resp = (HttpWebResponse)rq.GetResponse();
-        using(StreamReader s = new StreamReader(resp.GetResponseStream())) {
-            for(int i = 0; i < 9; i++){
-                s.Read();
-            }
-            string output = "";
-            char ch = (char)s.Read();
-            while (ch != '\"'){
-                if(ch == '\\')
-                    ch = (char)s.Read();
-                output += ch;
-                ch = (char)s.Read();
-            }
-            return output;
-        }
+        var lines = File.ReadAllLines("catfacts.txt");
+        
+        int rand = new Random().Next(lines.Length);
+        return lines[rand];
     }
     [STAThread]
     public static void Main(){
         while(true){
+            
             Clipboard.SetText(getCatFact());
-            Thread.Sleep(500);
+            Thread.Sleep(1000);
         }
     }
 }
